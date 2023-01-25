de75754bd8abba7ed527f6777ad965efe87fd36f
Zicio <zicio@yandex.ru>
2023-01-25 16:32:29 +0400

    Started testing Next.js 13

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,23 @@
-import './globals.css'
+import "./globals.css";
+import React from "react";
+import { Roboto } from "@next/font/google";
+
+const font = Roboto({ weight: "400" });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+    <html lang="ru">
+      <head>
+        <title>Main page</title>
+      </head>
+      <body>
+        <div className={font.className}>Layout</div>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
